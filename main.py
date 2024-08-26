826988357316d3a7958ebbd660611ff445051a92
Kevem <kevem2010@hotmail.com>
2024-08-26 13:42:15 -0300

    update

--- main.py
+++ main.py
@@ -1,30 +1,41 @@
-from extrai_tabelas import extrai_tabelas_da_url
+from pandas import read_html, to_numeric, DataFrame
+from requests import get
+from io import StringIO
 import matplotlib.pyplot as plt
 import numpy as np
-from os import listdir
-import pandas as pd
 import re
-    
+
+
+# Extrai a Tabela de Confrontos de cada Edição do Brasileirão de 2013 até 2023
+def extrai_tabelas_da_url(links: list):
+    lista_dataframe = []
+    for link in links:
+        requisicao = get(link)
+        lista_tabelas = read_html(StringIO(requisicao.text))
+
+        # Filtra a tabela de Confronto pela sua dimensão e transforma em um arquivo .xlsx
+        for tabela in lista_tabelas:
+            if tabela.shape == (20, 11):
+                dataframe = DataFrame(tabela)
+                lista_dataframe.append(dataframe)
+    return lista_dataframe
                 
 urls = [
         f"https://pt.wikipedia.org/wiki/Campeonato_Brasileiro_de_Futebol_de_{ano}_-_Série_A" 
         for ano in range(2013, 2024)
        ]
 
-extrai_tabelas_da_url(urls)
-
-caminho = './Tabelas'
 
 pontos_por_temporada = []
+vitorias_por_temporada = []
+
 # Percorrendo a pasta Tabelas.
-for arquivo in listdir(caminho):
-    soma = 0
+for arquivo in extrai_tabelas_da_url(urls):
 
     # Lendo e transformando o arquivo .csv em Dataframe.
-    arquivo_atual = pd.read_csv(caminho + '/' + arquivo)
+    arquivo_atual = arquivo
 
     # Deletando as linhas e colunas irrelevantes.
-    arquivo_atual = arquivo_atual.drop(axis=1, labels=["Unnamed: 0", "J", "V", "E", "D", "GP", "GC", "SG"])
     arquivo_atual = arquivo_atual.drop(axis=0, index=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 18, 19])
     
     if "Classificação ou descenso" in arquivo_atual.columns:
@@ -34,20 +45,26 @@ for arquivo in listdir(caminho):
     
     # Removendo possiveis caracteres que não sejam numeros da coluna Pts, transformando em numeros e somando posteriormente
     arquivo_atual["Pts"] = arquivo_atual["Pts"].apply(lambda pontos: re.sub(r'\D', '', str(pontos)))
-    arquivo_atual["Pts"] = pd.to_numeric(arquivo_atual["Pts"])
-    soma = arquivo_atual["Pts"].sum()
+    arquivo_atual["Pts"] = to_numeric(arquivo_atual["Pts"])
 
-    pontos_por_temporada.append(int(soma))
+    pontos_por_temporada.append(int(arquivo_atual["Pts"]))
 
-x = np.arange(start=2014, stop=2024)
-y = pontos_por_temporada
+    arquivo_atual["V"] = to_numeric(arquivo_atual["V"])
+    vitorias_por_temporada.append(int(arquivo_atual["V"]))
 
-fig, ax = plt.subplots()
-ax.plot(x, y)
-ax.set_xticks(x)
 
-plt.xlabel('Temporada')
-plt.ylabel('Pontuação')
-plt.title('Pontos do 1° time na zona de rebaixamento por Temporada.')
-plt.grid(visible=True)
-plt.show()
+num_elementos = (len(pontos_por_temporada) + len(vitorias_por_temporada)) / 2
+
+media_pontos = sum(pontos_por_temporada) / num_elementos
+media_vitorias = sum(vitorias_por_temporada) / num_elementos
+
+variancia_pontos = sum([(valor - media_pontos)**2 for valor in pontos_por_temporada]) / num_elementos
+variancia_vitorias = sum([(valor - media_vitorias)**2 for valor in vitorias_por_temporada]) / num_elementos
+
+desvio_padrao_pontos = variancia_pontos ** 0.5
+desvio_padrao_vitorias = variancia_vitorias ** 0.5
+
+covariancia = sum([(vitorias_por_temporada[i] - media_vitorias) * (pontos_por_temporada[i] - media_pontos) for i in range(0, 10)]) / num_elementos
+
+correlação = covariancia / (desvio_padrao_pontos * desvio_padrao_vitorias)
+
